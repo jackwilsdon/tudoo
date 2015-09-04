8138e6ace7084347e8a1b04f965afbf15af4a131
Jack Wilsdon <jack.wilsdon2@gmail.com>
2015-09-04 13:29:16 +0100

    Do not use function to generate clean tasks and remove bower dependency from dev

--- gulpfile.js
+++ gulpfile.js
@@ -15,14 +15,13 @@ var sourceDirectory = './src',
     distDirectory   = './dist',
     bowerDirectory  = join(sourceDirectory, 'lib');
 
-function cleanTask(glob) {
-  return function() {
-    return del(glob);
-  };
-}
+gulp.task('clean:dev', function() {
+  return del(devDirectory);
+});
 
-gulp.task('clean:dev', cleanTask(devDirectory));
-gulp.task('clean:dist', cleanTask(distDirectory));
+gulp.task('clean:dist', function() {
+  return del(distDirectory);
+});
 
 gulp.task('bower', function() {
   return plugins.bower(bowerDirectory);
@@ -43,7 +42,7 @@ gulp.task('sync', function() {
   return gulp.src('bower.json').pipe(plugins.configSync()).pipe(gulp.dest('.'));
 });
 
-gulp.task('dev', [ 'clean:dev', 'bower', 'copy:dev' ]);
+gulp.task('dev', [ 'clean:dev', 'copy:dev' ]);
 gulp.task('dist', [ 'clean:dist', 'usemin:dist' ]);
 
 gulp.task('server:dev', [ 'dev' ], function() {
